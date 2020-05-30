728e4ea289c3cd88c01d2c1aeb1cf6a36c0489f8
David <david.izosimov@gmail.com>
2020-05-30 17:53:00 +0200

    Adds extras component with youtube player API
    - Adds GIF for landing page
    - Adds pacman loader svg to assets and LazyLoading component

--- src/components/LazyLoading/index.js
+++ src/components/LazyLoading/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import styles from './styles.module.scss'
+
+import { ReactComponent as PacMan } from '../../Assets/svgs/pacMan.svg'
 
 const LazyLoading = () => {
-  return <p>Loading</p>;
+  return (
+    <div className={styles.position}>
+      <PacMan />;
+    </div>
+  )
 };
 
 export default LazyLoading;
